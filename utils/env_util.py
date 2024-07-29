72912aeebb069289b9318e466eb4d9b975a753b1
Zhigao Zeng <zengzhigao@stu.csust.edu.cn>
2024-07-29 21:52:48 +0800

    Create env_util.py

---
+++ utils/env_util.py
@@ -0,0 +1,27 @@
+import random
+import cv2
+import numpy as np
+
+
+def adjust_action(t, threshold):
+    # Explore as much as possible in the early stage
+    if t < threshold:
+        return np.array([0.1, 1, 0])
+
+    rn = random.randint(0, 9)
+    if rn in [0, 9]:
+        return np.array([0, 0, 0])
+    elif rn in [1, 2]:
+        return np.array([0, random.random(), 0])
+    elif rn in [3, 4]:
+        return np.array([-random.random(), 0, 0])
+    elif rn in [5, 6]:
+        return np.array([random.random(), 0, 0])
+    elif rn in [7, 8]:
+        return np.array([0, 0, random.random()])
+
+def adjust_obs(obs, size):
+    obs = cv2.resize(obs, dsize=size, interpolation=cv2.INTER_CUBIC)
+    return obs.astype('float32') / 255.
+
+# def get_data(filename, )
